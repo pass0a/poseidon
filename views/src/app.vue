67b5fd4613c97947622adf6dad6dde85e70b1bdf
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-05-31 09:55:35 +0800

    add someone

--- views/src/app.vue
+++ views/src/app.vue
@@ -4,32 +4,32 @@
   </div>
 </template>
 <script lang="ts">
-let pack = require("./ext/pack/index.js");
+import * as pack from "./ext/pack/index";
 let pis = new pack.inputStream();
 let pos = new pack.outputStream();
 
 import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class App extends Vue {
-    private ws: any = null;
-    private count:any=0;
-    private created() {
-        pis.on("data", (data: any) => {
-            this.ws.send(data);
-        });
-        pos.on("data", (data: any) => {
-            console.log(data.type);
-        });
-        this.ws = new WebSocket("ws://127.0.0.1:6001");
-        this.ws.onopen = () => {
-            this.ws.binaryType = "arraybuffer";
-        };
-        this.ws.onmessage = (frm: any) => {
-            pos.push(frm.data);
-        };
-        this.ws.onclose = () => {
-            console.log("close websocket!!!");
-        };
-    }
+  private ws: any = null;
+  private count: any = 0;
+  private created() {
+    pis.on("data", (data: any) => {
+      this.ws.send(data);
+    });
+    pos.on("data", (data: any) => {
+      console.log(data.type);
+    });
+    this.ws = new WebSocket("ws://127.0.0.1:6001");
+    this.ws.onopen = () => {
+      this.ws.binaryType = "arraybuffer";
+    };
+    this.ws.onmessage = (frm: any) => {
+      pos.push(frm.data);
+    };
+    this.ws.onclose = () => {
+      console.log("close websocket!!!");
+    };
+  }
 }
 </script>
