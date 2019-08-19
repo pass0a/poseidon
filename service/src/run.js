05b5a7217be882b22acf39cdc2bb7d4c5065628d
liu <losmemo@gmail.com>
2019-08-19 16:22:09 +0800

    add ftp upload
    ftp upload only master
    
    add cvip cp
    
    fixed azure-pipelines.yml path
    
    fixed install node_modules/@passoa
    
    build poseidon&reveal on step
    
    add adb package
    
    add adb pack

--- service/src/run.js
+++ service/src/run.js
@@ -1,3 +1,4 @@
+require('child_process').exec('browser/browser --url=http://127.0.0.1:6003', { shell: false, detached: true });
 require('./db/index.js');
 require('./app.js');
-require('child_process').exec('browser/browser --url=http://127.0.0.1:6003', { shell: false, detached: true });
+
