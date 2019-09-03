a971dd937cd74e97bd6a24bb8ca98245ce15a254
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-09-03 16:55:00 +0800

    update favicon.ico&fixed connect:close for chrome

--- service/src/run.js
+++ service/src/run.js
@@ -1,4 +1,4 @@
-require('child_process').exec('browser/browser --url=http://127.0.0.1:6003', { shell: false});
+require('child_process').exec('browser/browser --url=http://127.0.0.1:6003', { shell: false });
+require('./db/index.js');
 require('./db/index.js');
 require('./app.js');
-
