36503a3ae982a6745101fa47db78391a56c40ea4
unknown <huangzepeng@hangsheng.cn>
2019-10-31 11:34:16 +0800

    修复结束测试后串口没有关闭问题

--- service/src/candbc/main.ts
+++ service/src/candbc/main.ts
@@ -1,57 +1,86 @@
-// import * as Pcan from '@passoa/pcan';
-
-// let config = {
-//     baudrate : 0x432F,
-//     hardware_type : 0x01,
-//     io_port : 0x0100,
-//     interrupt : 3
-// };
-
-// console.log(parseInt("0100",16));
-
-// console.log(Pcan.initPcan(config,(ev,data)=>{
-//     console.log(ev,data);
-// }));
-
+import * as Pcan from '@passoa/pcan';
 import * as util from "util";
 import * as fs from "fs";
 
-// let dbc_file:any = JSON.parse(new util.TextDecoder().decode(fs.readFileSync("D:/projectList/poseidon/binary/service/dist/123.json")));
-// for(let i=0;i<dbc_file.messages.length;i++){
-//     if(dbc_file.messages[i].name == "DATC_PE_03"){
-//         console.log(dbc_file.messages[i].id);
-//         console.log(dbc_file.messages[i].dlc);
-//         let buffer = new Uint8Array(dbc_file.messages[i].dlc);
-//         for(let j=0;j<dbc_file.messages[i].signals.length;j++){
-//             if(dbc_file.messages[i].signals[j].name=="DATC_AutoDisp"){
-//                 console.log(dbc_file.messages[i].signals[j]);
-//             }
-//         }
-//     }
-// }
+let config = {
+    baudrate : 0x432F,
+    hardware_type : 0x01,
+    io_port : 0x0100,
+    interrupt : 3
+};
+let messagesList:any = {};
 
-let buffer = new Uint8Array([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00]);
-let inf:any = 0;
-let jl:any = [];
-for(let i=0;i<buffer.length;i++){
-    inf = buffer[i] << 8*i | inf
-    for(let j=0;j<8;j++){
-        jl.push((buffer[i]>>(7-j)) & 0x01);
+Pcan.initPcan(config,(ev,data)=>{
+    console.log(ev,data);
+    main();
+    Pcan.send(messagesList["DATC_PE_03"].data,messagesList["DATC_PE_03"].id);
+    Pcan.uninitPcan();
+});
+
+function main() {
+    let dbc_file:any = JSON.parse(new util.TextDecoder().decode(fs.readFileSync("D:/projectList/poseidon/binary/service/dist/123.json")));
+    for(let i=0;i<dbc_file.messages.length;i++){
+        messagesList[dbc_file.messages[i].name] = {
+            data : new Uint8Array(dbc_file.messages[i].dlc),
+            id : dbc_file.messages[i].id
+        }
+        if(dbc_file.messages[i].name == "DATC_PE_03"){
+            console.log(dbc_file.messages[i].id);
+            console.log(dbc_file.messages[i].dlc);
+            for(let j=0;j<dbc_file.messages[i].signals.length;j++){
+                if(dbc_file.messages[i].signals[j].name=="DATC_AutoDisp"){
+                    console.log(dbc_file.messages[i].signals[j]);
+                    if(dbc_file.messages[i].signals[j].endianess=="motorola"){
+                        messagesList[dbc_file.messages[i].name].data = disposedForMotorola(messagesList[dbc_file.messages[i].name],dbc_file.messages[i].signals[j],0x01);
+                        console.log(messagesList[dbc_file.messages[i].name]);
+                    }
+                }
+            }
+        }
     }
 }
-let start_bit = 7,len = 12;
-let start_idx = 8*Math.floor(start_bit/8) + (7-(start_bit%8));
-console.log(Math.floor(start_bit/8));
-console.log(start_bit%8);
-console.log(start_idx);
-let akt = 998;
-for(let i=start_idx;i<start_idx+len;i++){
-    jl[i] = (akt >> (start_idx+len-1-i)) & 0x01;
-}
-let op:any = [];
-let dl = 0;
-for(let i = 0;i<jl.length;i++){
-    
+
+function disposedForMotorola(msg:any, info:any, data:any) {
+    let buffer = msg.data;
+    // motorola
+    let jl:any = [];
+    for(let i=0;i<buffer.length;i++){
+        for(let j=0;j<8;j++){
+            jl.push((buffer[i]>>(7-j)) & 0x01);
+        }
+    }
+    let start_bit = info.startbit,len = info.bitlength;
+    let start_idx = 8*Math.floor(start_bit/8) + (7-(start_bit%8));
+    // console.log(Math.floor(start_bit/8));
+    // console.log(start_bit%8);
+    // console.log(start_idx);
+    let akt = data;
+    for(let i=start_idx;i<start_idx+len;i++){
+        jl[i] = (akt >> (start_idx+len-1-i)) & 0x01;
+    }
+    let op:any = [];
+    for(let i = 0;i<jl.length;i+=8){
+        op.push((jl[i]<<7 | jl[i+1]<<6 | jl[i+2]<<5 | jl[i+3]<<4 | jl[i+4]<<3 | jl[i+5]<<2 | jl[i+6]<<1 | jl[i+7]));
+    }
+    console.log(op);
+    return new Buffer(op);
 }
 
-console.log(jl);
+
+
+// // inter
+// let jt:any = [];
+// for(let i=0;i<buffer.length;i++){
+//     for(let j=0;j<8;j++){
+//         jt.push((buffer[i]>>j) & 0x01);
+//     }
+// }
+// console.log(jt);
+// for(let i=start_bit;i<start_bit+len;i++){
+//     jt[i] = (akt >> i-start_bit) & 0x01;
+// }
+// let tp:any = [];
+// for(let i = 0;i<jt.length;i+=8){
+//     tp.push((jt[i] | jt[i+1]<<1 | jt[i+2]<<2 | jt[i+3]<<3 | jt[i+4]<<4 | jt[i+5]<<5 | jt[i+6]<<6 | jt[i+7]<<7));
+// }
+// console.log(tp);
