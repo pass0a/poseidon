5004ffcea99b1776c7d5f29fd3e6da1aa4b84738
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-11-13 10:37:37 +0800

    v3.2.6 fixed version

--- service/pmconfig.js
+++ service/pmconfig.js
@@ -4,10 +4,6 @@ module.exports = [
 		msvc: 'https://github.com/pass0a/cvip/releases/download/v1.0.2/cvip-msvc-v1.0.2.tar.gz'
 	},
 	{
-		name: 'dbcc',
-		msvc: 'https://github.com/losper/dbcc/releases/download/v1.0.0/dbcc-msvc-v1.0.0.tar.gz'
-	},
-	{
 		name: 'pcan',
 		msvc: 'https://github.com/pass0a/pcan/releases/download/v1.0.0/pcan-msvc-v1.0.0.tar.gz'
 	}
