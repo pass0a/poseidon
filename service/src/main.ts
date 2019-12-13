7a431bf2f8c67e7b234f863bec32a1f887107dac
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-12-13 18:24:07 +0800

    add debuglog

--- service/src/main.ts
+++ service/src/main.ts
@@ -1,3 +1,16 @@
+const util = require('util');
+let debuglog = util.debuglog;
+util.debuglog = function(set: string) {
+	if (set === 'stream') {
+		return function() {
+			var msg = util.format.apply(util, arguments);
+			console.error(`[${set}]: ${msg}`);
+		};
+	}
+	return debuglog(set);
+};
+
+//console.logmode('rotating', 'passoa', 'passoa.log', 1024 * 1024 * 5, 1);
 require('./httpop/main');
 require('./dbop/main');
 require('./linkmgr/main');
